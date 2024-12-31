58e28962e6bb4bcdebf5ca609eb84b42bde4a9b4
bird_d <relay198@gmail.com>
2024-12-31 00:27:49 -0500

    Improve drop to slot pop-up look/behavior

--- src/panels/PanelDraft/DraftImageSlotPickerPopup.tsx
+++ src/panels/PanelDraft/DraftImageSlotPickerPopup.tsx
@@ -6,7 +6,18 @@ import { observer } from 'mobx-react-lite'
 import { useMemo } from 'react'
 
 import { Button } from '../../csuite/button/Button'
+import { MenuDivider } from '../../csuite/dropdown/MenuDivider'
 import { isFieldImage } from '../../csuite/fields/WidgetUI.DI'
+
+function formatFieldPath(text: string): string {
+   const builder = []
+
+   const t = text.split('$.').join().split('.')
+   builder.push(t.pop())
+   builder.push(t.pop())
+
+   return builder.join('>')
+}
 
 export const DraftImageSlotPickerUI = observer(function DraftImageSlotPickerUI_({
    // own
@@ -27,21 +38,56 @@ export const DraftImageSlotPickerUI = observer(function DraftImageSlotPickerUI_(
       })
       return allImageFields
    }, [])
+
+   let lastParent = ''
+
    return (
-      <div {...rest}>
-         {imageFields.map((imgField, i) => (
-            <div key={i}>
-               <Button
-                  onClick={() => {
-                     imgField.value = image
-                     cushy.showConfettiAndBringFun()
-                     stop()
-                  }}
-               >
-                  {imgField.path}: {imgField.labelText}
-               </Button>
-            </div>
-         ))}
+      <div tw='flex flex-col' {...rest}>
+         {imageFields.map((imgField, i) => {
+            let addDivider = false
+
+            const parentPath = imgField.path.split('.')
+            while (parentPath.length > 3) {
+               parentPath.pop()
+            }
+
+            const topParent = draft.form?.getFieldAt(parentPath.join('.'))
+
+            console.log('[FD]: FEF: ', topParent)
+
+            if (lastParent != topParent?.config.label) {
+               lastParent = topParent?.config.label
+               if (i != 0) {
+                  addDivider = true
+               }
+            }
+
+            return (
+               <>
+                  {addDivider && <MenuDivider />}
+                  <div key={i}>
+                     <Button
+                        borderless
+                        icon={addDivider && topParent ? topParent.config.icon : 'mdiChevronRight'}
+                        expand
+                        subtle
+                        // TODO(bird_d/ui/tooltips) : Really fucking annoying until tooltips are fixed
+                        // tooltip={imgField.config.tooltip ?? '' + '\n' + imgField.path}
+                        // tw='flex-1 w-full'
+                        onClick={() => {
+                           imgField.value = image
+                           // cushy.showConfettiAndBringFun()
+                           stop()
+                        }}
+                     >
+                        {addDivider && topParent ? `${topParent.config.label}: ` : ''}
+                        {formatFieldPath(imgField.path)}
+                        <span tw='ml-5 flex-1 text-right opacity-50'>{imgField.labelText}</span>
+                     </Button>
+                  </div>
+               </>
+            )
+         })}
       </div>
    )
 })
